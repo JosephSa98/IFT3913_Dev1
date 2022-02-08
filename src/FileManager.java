d91c040328632a9b332588248258818776a7c3b6
JosephSa98 <72052441+JosephSa98@users.noreply.github.com>
2022-02-08 16:37:17 -0500

    Count non-emp. lines & lines of comments (TEST)
    2 fonctions:
    Trouver le nombre de ligne qui ne sont pas vide
    Trouver le CLOC dans la classe (lines of comment)

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,7 +1,59 @@
 public class FileManager{
-    public static void main(String[] args){
-        System.out.println("WELCOME!!!");
-		System.out.println("Hello World!");
+    public static void main(String[] args) {
+        String[] linesOfcode = {"/*hi",
+                "cava*/" ,
+                "bien" ,
+                "//et" ,
+                "toi" ,
+                "",
+                "",
+                "//merci",
+                "",
+                "",
+                "",
+                "",
+                "fin"};
+        linesOfCommentInClass(linesOfcode);
+        nonEmptyLinesOfCode(linesOfcode);
+    }
+
+    /*
+     * Trouver le CLOC dans la classe
+     * */
+    public static void linesOfCommentInClass(String[] linesOfcode){
+        int comments = 0;
+
+        for (int i = 0; i < linesOfcode.length; i++) {
+            if(linesOfcode[i].contains("/*")){
+                while (!(linesOfcode[i].contains("*/")) && (i < linesOfcode.length - 1)){
+                    comments++;
+                    i++;
+                }
+                if (linesOfcode[i].contains("*/")){
+                    comments++;
+                }
+            }else if (linesOfcode[i].contains("//")){
+                comments++;
+            }
+        }
+        System.out.println("Lines of comment: " + comments);
+    }
+
+    /*
+    * Trouver le nombre de ligne qui ne sont pas vide
+    * */
+    public static void nonEmptyLinesOfCode (String[] linesOfcode){
+        int nonEmptyLines = 0;
+
+        for (int i = 0; i < linesOfcode.length; i++) {
+            if (linesOfcode[i].isBlank()){
+                continue;
+            }else {
+                nonEmptyLines++;
+            }
+        }
+        System.out.println("Number of non empty lines: " + nonEmptyLines);
     }
 
 }
+
