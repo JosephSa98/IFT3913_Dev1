f0336c403f138d0db2e350a41398a36c738334a7
JosephSa98 <72052441+JosephSa98@users.noreply.github.com>
2022-02-09 16:37:07 -0500

    CSV file writer (phase 1)
    function that will produce report of wanted data and send it to another function that will write in a csv file the given input

--- src/CreateCSV.java
+++ src/CreateCSV.java
@@ -1,30 +1,67 @@
 import javax.swing.*;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class CreateCSV {
-    public static void main (String[] args){
-        String record = "this is a test";
-        String record2 = "WORKING!";
-        String filePath = "csvTester.txt";
 
-        saveRecord(record,record2,filePath);
+/*APPEL LA FONCTION REPORTCLASSESINPUT() POUR GENERER UN ARRAY CONTENANT LES INFOS*/
+/*ENSUITE FAIT APPEL A WRITECSVFILE QUI VA ECRIRE DANS LE FICHIER CSV LES INFOS ET LEURS NOMS DE COLOMNES*/
 
-    }
-    public static void saveRecord(String record,String record2,String filePath){
+//    public void produceData() {
+//
+//        String[] results = this.reportClassesInput();
+//        this.writeCSVFile(data,true);
+//    }
+
+
+/*CHERCHE L'INFORMATION DES CLASSES CALUCULANT LOC, CLOC ETC... PAR NOTRE AUTOMATE :) ET LES INSERE DANS LE ARRAY "DATA"*/
+
+//    public String[] reportClassesInput(){
+//        String[] data = new String[this.classes.length+1];
+//        data[0] = "chemin ," + "class ," + "classe_LOC ," + "classe_CLOC ," + "classe_DC \n";
+//        for (var i=0; i < this.classes.length; i++) {
+//            data[i+1] = this.absolutePaths[i] + "," + this.classNames[i] + "," +
+//                    classe_LOC(this.classes[i]) + "," + classe_CLOC(this.classes[i]) +
+//                    "," + classe_DC(this.classes[i])+ "\n";
+//        }
+//        return data;
+//    }
+
+    /*ECRIT DANS LE FICHIER CSV LES INFOS RECU PAR PRODUCEDATA()*/
+    public static void writeCSVFile(String[] data, boolean append){
+
+        String csvClassesPath = "CSV/classes.csv";
+
         try {
-            FileWriter fw = new FileWriter(filePath,false);
-            BufferedWriter bw = new BufferedWriter(fw);
+            FileWriter csvWriter = new FileWriter(csvClassesPath, append);
+            BufferedWriter bw = new BufferedWriter(csvWriter);
             PrintWriter pw = new PrintWriter(bw);
 
-            pw.println(record+", "+record2);
-            pw.flush();
+            //data[0] = "chemin ,"+"class ,"+"classe_LOC ,"+"classe_CLOC ,"+"classe_DC"+"\n";
+
+
+            int i = 0;
+            while (i < data.length) {
+
+                if(i < data.length-1){
+                    pw.print(data[i] + ", ");
+                }
+                else{
+                    pw.println(data[i] + "\n");
+                }
+                pw.flush();
+                i++;
+            }
             pw.close();
-        }
-        catch (Exception E){
+
+        } catch (IOException e) {
             JOptionPane.showMessageDialog(null,"Record NOT saved!");
         }
+    }
+
+    public static void main (String[] args){
+        /*HARD CODED STRING TO TEST WRITECSVFILE FUNCTION*/
+//        String[] data = {"a","b","c","d","e"};
+//        writeCSVFile(data,true);
+
     }
 }
